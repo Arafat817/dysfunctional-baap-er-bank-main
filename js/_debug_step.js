b4e9035dfa8ce6159bddd587912ad4e0a3e66f6e
Arafat817 <gr1arafat@gmail.com>
2023-04-15 16:20:22 +0600

    first commit

---
+++ js/_debug_step.js
@@ -0,0 +1,9 @@
+/*
+Debug steps
+1.Go to the live website and try to reproduce the error.
+2.check the console of the live website to see any error.
+3.read exception thoroughly in detail
+4.If the error is new to you: copy the text of the error and search online. 
+
+
+*/
