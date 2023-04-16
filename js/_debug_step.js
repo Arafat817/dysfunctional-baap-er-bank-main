eb6fce4853c572827b689d563d3a9f5205321b25
Arafat817 <gr1arafat@gmail.com>
2023-04-16 15:41:18 +0600

    debug complete

--- js/_debug_step.js
+++ js/_debug_step.js
@@ -4,6 +4,13 @@ Debug steps
 2.check the console of the live website to see any error.
 3.read exception thoroughly in detail
 4.If the error is new to you: copy the text of the error and search online. 
+5. if a line number is showing , click on that line to see the code.
+6. add breakpoint, re run the code( refresh the website) and see the whether you are the
+ getting expected values. 
+ 6.1 . if you are not getting the expected values , check which values you are not
+  getting. if needed a breakpoint in previous line.
+  6.2. if needed step into (f11) to a function. 
+
 
 
 */
